fb3559770a3a9395899896ce0838fa79f538b842
Bálint Juhász <juhasz.balint.bebe@gmail.com>
2017-05-05 21:18:52 +0200

    first working build with Agency api

---
+++ GtfsManager/Migrations/20170505190206_InitialCreate.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GtfsManager.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Agencies",
+                columns: table => new
+                {
+                    agency_id = table.Column<string>(nullable: false),
+                    agency_email = table.Column<string>(nullable: true),
+                    agency_fare_url = table.Column<string>(nullable: true),
+                    agency_lang = table.Column<string>(nullable: true),
+                    agency_name = table.Column<string>(nullable: false),
+                    agency_phone = table.Column<string>(nullable: true),
+                    agency_timezone = table.Column<string>(nullable: false),
+                    agency_url = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Agencies", x => x.agency_id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Calendar",
+                columns: table => new
+                {
+                    primary_key = table.Column<string>(nullable: false),
+                    end_date = table.Column<string>(nullable: false),
+                    friday = table.Column<string>(nullable: false),
+                    monday = table.Column<string>(nullable: false),
+                    saturday = table.Column<string>(nullable: false),
+                    service_id = table.Column<string>(nullable: false),
+                    start_date = table.Column<string>(nullable: false),
+                    sunday = table.Column<string>(nullable: false),
+                    thursday = table.Column<string>(nullable: false),
+                    tuesday = table.Column<string>(nullable: false),
+                    wednesday = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Calendar", x => x.primary_key);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Routes",
+                columns: table => new
+                {
+                    route_id = table.Column<string>(nullable: false),
+                    agency_id = table.Column<string>(nullable: true),
+                    route_color = table.Column<string>(nullable: true),
+                    route_desc = table.Column<string>(nullable: true),
+                    route_long_name = table.Column<string>(nullable: false),
+                    route_short_name = table.Column<string>(nullable: false),
+                    route_text_color = table.Column<string>(nullable: true),
+                    route_type = table.Column<string>(nullable: false),
+                    route_url = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Routes", x => x.route_id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Stops",
+                columns: table => new
+                {
+                    stop_id = table.Column<string>(nullable: false),
+                    location_type = table.Column<string>(nullable: true),
+                    parent_station = table.Column<string>(nullable: true),
+                    stop_code = table.Column<string>(nullable: true),
+                    stop_desc = table.Column<string>(nullable: true),
+                    stop_lat = table.Column<string>(nullable: false),
+                    stop_lon = table.Column<string>(nullable: false),
+                    stop_name = table.Column<string>(nullable: false),
+                    stop_timezone = table.Column<string>(nullable: true),
+                    stop_url = table.Column<string>(nullable: true),
+                    wheelchair_boarding = table.Column<string>(nullable: true),
+                    zone_id = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Stops", x => x.stop_id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Stoptimes",
+                columns: table => new
+                {
+                    primary_key = table.Column<string>(nullable: false),
+                    arrival_time = table.Column<string>(nullable: false),
+                    departure_time = table.Column<string>(nullable: false),
+                    drop_off_type = table.Column<string>(nullable: true),
+                    pickup_type = table.Column<string>(nullable: true),
+                    shape_dist_traveled = table.Column<string>(nullable: true),
+                    stop_headsign = table.Column<string>(nullable: true),
+                    stop_id = table.Column<string>(nullable: false),
+                    stop_sequence = table.Column<string>(nullable: false),
+                    timepoint = table.Column<string>(nullable: true),
+                    trip_id = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Stoptimes", x => x.primary_key);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Trips",
+                columns: table => new
+                {
+                    trip_id = table.Column<string>(nullable: false),
+                    bikes_allowed = table.Column<string>(nullable: true),
+                    block_id = table.Column<string>(nullable: true),
+                    direction_id = table.Column<string>(nullable: true),
+                    route_id = table.Column<string>(nullable: false),
+                    service_id = table.Column<string>(nullable: false),
+                    shape_id = table.Column<string>(nullable: true),
+                    trip_headsign = table.Column<string>(nullable: true),
+                    trip_short_name = table.Column<string>(nullable: true),
+                    wheelchair_accessible = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Trips", x => x.trip_id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Agencies");
+
+            migrationBuilder.DropTable(
+                name: "Calendar");
+
+            migrationBuilder.DropTable(
+                name: "Routes");
+
+            migrationBuilder.DropTable(
+                name: "Stops");
+
+            migrationBuilder.DropTable(
+                name: "Stoptimes");
+
+            migrationBuilder.DropTable(
+                name: "Trips");
+        }
+    }
+}
---
+++ GtfsManager/Migrations/GtfsContextModelSnapshot.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using GTFSManagerApi.Models;
+
+namespace GtfsManager.Migrations
+{
+    [DbContext(typeof(GtfsContext))]
+    partial class GtfsContextModelSnapshot : ModelSnapshot
+    {
+        protected override void BuildModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.1")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("GTFSManagerApi.Models.AgencyItem", b =>
+                {
+                    b.Property<string>("agency_id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("agency_email");
+
+                    b.Property<string>("agency_fare_url");
+
+                    b.Property<string>("agency_lang");
+
+                    b.Property<string>("agency_name")
+                        .IsRequired();
+
+                    b.Property<string>("agency_phone");
+
+                    b.Property<string>("agency_timezone")
+                        .IsRequired();
+
+                    b.Property<string>("agency_url")
+                        .IsRequired();
+
+                    b.HasKey("agency_id");
+
+                    b.ToTable("Agencies");
+                });
+
+            modelBuilder.Entity("GTFSManagerApi.Models.CalendarItem", b =>
+                {
+                    b.Property<string>("primary_key")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("end_date")
+                        .IsRequired();
+
+                    b.Property<string>("friday")
+                        .IsRequired();
+
+                    b.Property<string>("monday")
+                        .IsRequired();
+
+                    b.Property<string>("saturday")
+                        .IsRequired();
+
+                    b.Property<string>("service_id")
+                        .IsRequired();
+
+                    b.Property<string>("start_date")
+                        .IsRequired();
+
+                    b.Property<string>("sunday")
+                        .IsRequired();
+
+                    b.Property<string>("thursday")
+                        .IsRequired();
+
+                    b.Property<string>("tuesday")
+                        .IsRequired();
+
+                    b.Property<string>("wednesday")
+                        .IsRequired();
+
+                    b.HasKey("primary_key");
+
+                    b.ToTable("Calendar");
+                });
+
+            modelBuilder.Entity("GTFSManagerApi.Models.RouteItem", b =>
+                {
+                    b.Property<string>("route_id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("agency_id");
+
+                    b.Property<string>("route_color");
+
+                    b.Property<string>("route_desc");
+
+                    b.Property<string>("route_long_name")
+                        .IsRequired();
+
+                    b.Property<string>("route_short_name")
+                        .IsRequired();
+
+                    b.Property<string>("route_text_color");
+
+                    b.Property<string>("route_type")
+                        .IsRequired();
+
+                    b.Property<string>("route_url");
+
+                    b.HasKey("route_id");
+
+                    b.ToTable("Routes");
+                });
+
+            modelBuilder.Entity("GTFSManagerApi.Models.StopItem", b =>
+                {
+                    b.Property<string>("stop_id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("location_type");
+
+                    b.Property<string>("parent_station");
+
+                    b.Property<string>("stop_code");
+
+                    b.Property<string>("stop_desc");
+
+                    b.Property<string>("stop_lat")
+                        .IsRequired();
+
+                    b.Property<string>("stop_lon")
+                        .IsRequired();
+
+                    b.Property<string>("stop_name")
+                        .IsRequired();
+
+                    b.Property<string>("stop_timezone");
+
+                    b.Property<string>("stop_url");
+
+                    b.Property<string>("wheelchair_boarding");
+
+                    b.Property<string>("zone_id");
+
+                    b.HasKey("stop_id");
+
+                    b.ToTable("Stops");
+                });
+
+            modelBuilder.Entity("GTFSManagerApi.Models.StoptimeItem", b =>
+                {
+                    b.Property<string>("primary_key")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("arrival_time")
+                        .IsRequired();
+
+                    b.Property<string>("departure_time")
+                        .IsRequired();
+
+                    b.Property<string>("drop_off_type");
+
+                    b.Property<string>("pickup_type");
+
+                    b.Property<string>("shape_dist_traveled");
+
+                    b.Property<string>("stop_headsign");
+
+                    b.Property<string>("stop_id")
+                        .IsRequired();
+
+                    b.Property<string>("stop_sequence")
+                        .IsRequired();
+
+                    b.Property<string>("timepoint");
+
+                    b.Property<string>("trip_id")
+                        .IsRequired();
+
+                    b.HasKey("primary_key");
+
+                    b.ToTable("Stoptimes");
+                });
+
+            modelBuilder.Entity("GTFSManagerApi.Models.TripItem", b =>
+                {
+                    b.Property<string>("trip_id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("bikes_allowed");
+
+                    b.Property<string>("block_id");
+
+                    b.Property<string>("direction_id");
+
+                    b.Property<string>("route_id")
+                        .IsRequired();
+
+                    b.Property<string>("service_id")
+                        .IsRequired();
+
+                    b.Property<string>("shape_id");
+
+                    b.Property<string>("trip_headsign");
+
+                    b.Property<string>("trip_short_name");
+
+                    b.Property<string>("wheelchair_accessible");
+
+                    b.HasKey("trip_id");
+
+                    b.ToTable("Trips");
+                });
+        }
+    }
+}
